7a0934803523b32d9146c4684e158557d6de38d7
ABsport <162340320+ABsport@users.noreply.github.com>
2024-03-05 21:03:28 +0545

    A website

---
+++ web.js
@@ -0,0 +1,16 @@
+
+function toggleSidebar() {
+    document.getElementById("mySidebar").style.width = "250px";
+
+    document.getElementById("overlay").style.display = "block";
+  
+  }
+
+  function closeSidebar() {
+    document.getElementById("mySidebar").style.width = "0";
+  
+    document.getElementById("overlay").style.display = "none";
+   
+  }
+
+
